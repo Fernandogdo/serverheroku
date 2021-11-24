0c8bb4b298fd30f9b353533212826b462cc07fb4
fernandogdo <fernandojaviergdo@gmail.com>
2021-11-24 14:59:17 -0500

    cambios whitenoise middleware

--- generadorCV/settings.py
+++ generadorCV/settings.py
@@ -86,7 +86,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhitenoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
