8c9b91d14242292f174b149980d247fe5b5586a4
fernandogdo <fernandojaviergdo@gmail.com>
2021-11-30 12:52:16 -0500

    cambios admin.py

--- cv_api/admin.py
+++ cv_api/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 from . models import *
 
 admin.site.register(ConfiguracionCv)
 # admin.site.register(Administrador)
-admin.site.register(Usuario)
+admin.site.register(Usuario, UserAdmin)
 admin.site.register(ConfiguracionCv_Personalizado)
 # admin.site.register(ConfiguracionCv_Personalizado)
